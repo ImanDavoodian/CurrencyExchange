375fdb0f54a47c507be4127eb4ac9e8183c75332
Iman Davoodian <imandavoodian@yahoo.com>
2024-01-13 14:15:33 +0330

    Finish

--- CurrencyExchangeApp/Program.cs
+++ CurrencyExchangeApp/Program.cs
@@ -2,16 +2,16 @@
 
 List<Tuple<string, string, double>> mockData = new List<Tuple<string, string, double>>()
 {
-    new Tuple<string, string, double>("USD", "EUR", 1.2),
-    new Tuple<string, string, double>("USD", "AWS", 0.84),
-    new Tuple<string, string, double>("USD", "JDP", 0.63),
-    new Tuple<string, string, double>("EUR", "AWS", 0.47),
-    new Tuple<string, string, double>("EUR", "CAD", 0.79),
-    new Tuple<string, string, double>("JDP", "CAD", 1.35),
-    new Tuple<string, string, double>("CAD", "AWS", 1.36),
-    new Tuple<string, string, double>("AWS", "LTC", 1.7)
+            new Tuple<string, string, double>("USD", "EUR", 0.9132),
+            new Tuple<string, string, double>("USD", "GBP", 0.7842),
+            new Tuple<string, string, double>("USD", "JPY", 144.93),
+            new Tuple<string, string, double>("EUR", "GBP", 0.8588),
+            new Tuple<string, string, double>("EUR", "CAD", 1.4681),
+            new Tuple<string, string, double>("JPY", "CAD", 0.92510),
+            new Tuple<string, string, double>("CAD", "GBP", 0.5850),
+            new Tuple<string, string, double>("GBP", "AUD", 1.9072)
 };
 CurrencyConverter currencyConverter = new CurrencyConverter();
 currencyConverter.UpdateConfiguration(mockData);
-var test = await currencyConverter.Convert("JDP", "LTC", 10);
+var test = await currencyConverter.Convert("USD", "CAD", 10);
 Console.WriteLine($"Final Result:{test}");
